bff6e8d90d09bf5cf60ae2e1905346a31387db90
mickael De PECKER <mickael@cymoz.Fr>
2015-10-29 11:01:04 +0100

    initial

---
+++ api/models/User.js
@@ -0,0 +1,63 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+var bcrypt = require('bcryptjs')
+module.exports = {
+
+    attributes: {
+        firstname: {
+            type: 'string',
+            required: true,
+            minLength: 3,
+            unique: true
+        },
+        email: {
+            type: 'email',
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: 'string',
+        },
+        token: {
+            type: 'text'
+        },
+        refreshToken: {
+            type: 'text'
+        },
+        toJSON: function(){
+            var obj = this.toObject();
+            delete obj.password;
+            delete obj.refreshToken;
+            delete obj.createdAt;
+            delete obj.updatedAt;
+            delete obj.token;
+        }
+    },
+    beforeCreate: function (value,next){
+        bcrypt.genSalt(10, function(err,salt){
+            if(err) return next(err);
+            bcrypt.hash(value.password, salt,function(err,hash){
+                if(err) return next(err);
+                value.password = hash;
+                value.refreshToken = JwtHandler.generate({email:value.email});
+                next();
+            })
+        })
+    },
+    comparePassword : function(password,user,cb){
+        bcrypt.compare(password,user.password,function(err,match){
+            if(err)cb(err)
+            if(match){
+             cb(null,true)
+            }else{
+                cb(err)
+            }
+        })
+    }
+
+};
+
---
+++ config/jwt.js
@@ -0,0 +1,4 @@
+module.exports.jwt = {
+    jwt_secret: 'thisisasecretfromYnov',
+    jwt_ttl: 86400 // secondes -> 1 days
+}
